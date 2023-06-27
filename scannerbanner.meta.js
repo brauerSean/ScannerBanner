42ddce635a4b8d34883237b28ea9468a43349ea8
Sean Brauer <132447298+brauerSean@users.noreply.github.com>
2023-06-26 21:11:43 -0500

    test

---
+++ scannerbanner.meta.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Scanner Banner (GH)
+// @version      1.5
+// @description  Creates a banner to store your favorite barcodes. Ctrl + Alt shows/hides the banner. Press and hold ` while clicking to auto create barcodes of the text you click on.
+// @author       brausean
+// @match        https://*/*
+// @match        http://*/*
+// @grant        GM_listValues
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_deleteValue
+// @grant        GM_addValueChangeListener
+// @grant        GM_addStyle
+// @noframes
+// @require      https://cdn.jsdelivr.net/npm/jsbarcode@3.11.5/dist/JsBarcode.all.min.js
+// @require      https://raw.githubusercontent.com/brauerSean/ScannerBanner/main/ScannerBanner.js
+// ==/UserScript==
