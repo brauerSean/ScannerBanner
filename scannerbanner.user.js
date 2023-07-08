9c8bf24d4d0c87c3521a457483398fea3f945dd3
Lucas <lucasfrmr@gmail.com>
2023-07-08 16:59:22 -0500

    Updated scannerbanner.user.js

--- scannerbanner.user.js
+++ scannerbanner.user.js
@@ -1,8 +1,9 @@
 // ==UserScript==
 // @name         Scanner Banner (GH)
-// @version      2.0b
+// @version      2.0.1
 // @description  Creates a banner to store your favorite barcodes. Ctrl + Alt shows/hides the banner. Press and hold ` while clicking to auto create barcodes of the text you click on.
 // @author       brausean
+// @contributors luofarme
 // @match        https://*/*
 // @match        http://*/*
 // @grant        GM_listValues
